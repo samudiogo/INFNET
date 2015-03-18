937a0ebb04207cf62036bfb934481eba15e31dff
Samuel Diogo <samudiogo@gmail.com>
2015-03-18 17:52:13 -0300

    Update Principal.java

--- Java/Java II/ExArrayListComString/src/exercicio/programa/Principal.java
+++ Java/Java II/ExArrayListComString/src/exercicio/programa/Principal.java
@@ -8,71 +8,121 @@ public class Principal
 {
 	public static ArrayList<Aluno> list = Aluno.GenerateMonkyList();
 	
-	public static void main(String[] args)
-	{
-		ListFullNames();
-	}
-	
 	//aux methods:
-	
 	private static String[] splitNames(String fName)
 	{
-		return fName.split("\b");
+		return fName.split("\\s");
 	}
 	
-	// 1. Imprimir o vetor no console utilizando o "enhanced for"
-	public void ListNames()
+	public static void main(String[] args)
 	{
-		for(Aluno a: this.list)
+		System.out.println("\nList of names:");
+		ListNames();
+		System.out.println("\nList of names UPPER:");
+		ListnamesToUpper();
+		System.out.println("\nList of names Lower:");
+		ListnamesToLower();
+		System.out.println("\nList length names:");
+		ListNamesLength();
+		System.out.println("\nList full names:");
+		ListFullNames();
+		System.out.println("\nList names with 4 names:");
+		ListNamesWithFour();
+		System.out.println("\nList names with java names:");
+		ListNamesWithJava2();
+		System.out.println("\nList names with names starts with 'A':");
+		ListNamesStartingWithA();
+		System.out.println("\nList names with names ends with 'A':");
+		ListSurnamesEndindWithA();
+		System.out.println("\nList names with surnames starts with 'A':");
+		ListSurnameStartingWithA();
+	}
+
+	// 1. Imprimir o vetor no console utilizando o "enhanced for"
+	public static void ListNames()
+	{
+		for(Aluno a: list)
 			System.out.println(a.getNome());
 	}
 	
-	//2. Imprimir o vetor no console utilizando o "enhanced for". Todos as letras do nome maiúsculas (toUpperCase)
-	public void ListnamesToUpper()
+	//2. Imprimir o vetor no console utilizando o "enhanced for". Todos as letras do nome maiÃºsculas (toUpperCase)
+	public static void ListnamesToUpper()
 	{
-		for(Aluno a: this.list)
+		for(Aluno a: list)
 		{
 			System.out.println(a.getNome().toUpperCase());
 		}
 	}
 	
-	//3. Imprimir o vetor no console utilizando o "enhanced for". Todos as letras do nome minúsculas (toLowerCase)
-	public void ListnamesToLower()
+	//3. Imprimir o vetor no console utilizando o "enhanced for". Todos as letras do nome minÃºsculas (toLowerCase)
+	public static void ListnamesToLower()
 	{
-		for(Aluno a: this.list)
+		for(Aluno a: list)
 		{
 			System.out.println(a.getNome().toLowerCase());
 		}
 	}
 		
 	//4. Imprimir o vetor no console utilizando o "enhanced for", exibindo apenas o tamanho de cada String (length)
-	public void ListNamesLength()
+	public static void ListNamesLength()
 	{
-		for(Aluno a: this.list)
+		for(Aluno a: list)
 			System.out.println(a.getNome().length());
 	}
 	
 	//5. Imprimir o vetor no console utilizando o "enhanced for", concatenando o nome e o sobrenome de cada aluno (concat)
 	public static void ListFullNames()
 	{
-		for(Aluno a : list)
+		for(Aluno a: list)
 		{
-			String[] fName = splitNames(a.getNome());
-			String name = "";
-			int t = fName.length;
-			System.out.println(t);
-			System.out.println(name);
-				
-			
-		}
+			String fullName[] = splitNames(a.getNome());
+			String nomeCompleto = "";
+			int tam = fullName.length; 
+			if(tam > 1)
+				for(int i = 0; i <= (tam-1);++i)
+					nomeCompleto = nomeCompleto.concat(" ").concat(fullName[i]);
+			System.out.println(nomeCompleto);
+		}			
 	}
 	//6. Imprimir o vetor no console utilizando o "enhanced for", substituindo todas as letras "a" por "4" (replace)
-
-	//7. Imprimir o vetor no console utilizando o "enhanced for", substituindo todas as sequências "an" por "JAVA2" (replace)
-	//   [Utilize outra sequência de dois caracteres caso esta não exista no vetor]
+	public static void ListNamesWithFour()
+	{
+		for(Aluno a : list)
+			System.out.println(a.getNome().replace('a', '4'));
+	}
+	
+	//7. Imprimir o vetor no console utilizando o "enhanced for", substituindo todas as sequÃªncias "an" por "JAVA2" (replace)
+	//   [Utilize outra sequÃªncia de dois caracteres caso esta nÃ£o exista no vetor]
+	public static void ListNamesWithJava2()
+	{
+		for(Aluno a: list)
+			System.out.println(a.getNome().replace("an", "JAVA2"));
+	}
 	//8. Imprimir o vetor no console utilizando o "enhanced for", exibindo apenas os nomes iniciados com "A" (startsWith)
-
+	public static void ListNamesStartingWithA()
+	{
+		for(Aluno a: list)
+			if(a.getNome().startsWith("A"))
+				System.out.println(a.getNome());
+	}
 	//9. Imprimir o vetor no console utilizando o "enhanced for", exibindo apenas os sobrenomes terminados com "A" (endsWith)
+	public static void ListSurnamesEndindWithA()
+	{
+		for(Aluno a: list)
+			if(a.getNome().endsWith("a"))
+				System.out.println(a.getNome());
+	}
 
-	//10. Imprimir o vetor no console utilizando o "enhanced for", exibindo apenas os sobrenomes iniciados com "A" (startsWith)	
+	//10. Imprimir o vetor no console utilizando o "enhanced for", exibindo apenas os sobrenomes iniciados com "A" (startsWith)
+	public static void ListSurnameStartingWithA()
+	{
+		for(Aluno a: list)
+		{
+			String fullName[] = splitNames(a.getNome());
+			int t = fullName.length -1;
+			if(fullName[t].startsWith("A"))
+				System.out.println(fullName[t]);
+		}
+				
+	}
 }
